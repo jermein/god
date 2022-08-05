1b3917934b1ec27db9fd023210f611fa86d7bad3
jermein <110571749+jermein@users.noreply.github.com>
2022-08-05 10:43:14 -0500

    Add files via upload

---
+++ reescribir.py
@@ -0,0 +1,19 @@
+age = 35
+PI = 3.14159
+
+#se le esta asignado numeros a 2 variables
+
+maths_operations = 1 + 3 * 4 / 2 - 2
+print(maths_operations)
+
+#se le esta asignado una operacion matematica auna variable y se esta imprimiendo
+
+float_division = 12 / 3
+print(float_division)
+
+#se le esta asignado una division a una variable y se esta impriendo el resultado de esta
+
+integer_division = 12 // 3
+print(integer_division)
+
+#se le esta asignado una division entera a variable y se esta imprimiendo el resultado de esta
