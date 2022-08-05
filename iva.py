228ea1ba06a7e1472c2581df9d21b623bbc13177
jermein <110571749+jermein@users.noreply.github.com>
2022-08-05 10:43:35 -0500

    Add files via upload

---
+++ iva.py
@@ -0,0 +1,6 @@
+#//iva es de 0.19%
+precio = int(input("Digite precio del producto "))
+iva = precio * 0.19
+print ("El iva de su producto es: ",iva)
+preciototal = precio + iva 
+print("precio total es: ", preciototal)
